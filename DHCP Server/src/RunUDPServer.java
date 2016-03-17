67bf6ce3b1f23f0d236c468bf1bbd0eb9e44ea2e
PieterAppeltans <pieter@appeltans.eu>
2016-03-17 16:28:00 +0100

    Offer is working

--- DHCP Server/src/RunUDPServer.java
+++ DHCP Server/src/RunUDPServer.java
@@ -1,7 +1,7 @@
 public class RunUDPServer {
 	
 	public static void main(String[] args) throws Exception {
-		DHCPServer server = new DHCPServer("options.txt");
+		DHCPServer server = new DHCPServer(9000,new byte[]{0,0,0,1},new byte[]{0,0,0,(byte) 0Xff},new byte[]{10,12,0,0},10,1000,100);
 		server.run();
 	}
 	
